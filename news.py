1435ff74ba2359385a362724ad0f515a3187b9fe
ztttas1 <125969373+ztttas1@users.noreply.github.com>
2024-04-20 15:12:18 +0900

    Update news.py

--- news.py
+++ news.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/news')
 def index():
-    url = f'https://newsapi.org/v2/top-headlines?country=jp&apiKey=8d3db2a0f076426482dfc5b8787bad32}'
+    url = f'https://newsapi.org/v2/top-headlines?country=jp&apiKey=8d3db2a0f076426482dfc5b8787bad32'
     response = requests.get(url)
     articles = response.json().get('articles', [])
     return render_template('news.html', articles=articles)
